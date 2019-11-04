6c53fac8b565e816da73b7569e1779a48a93602e
LinkPhoenix <linkblue11@outlook.com>
2019-11-04 02:59:14 +0100

    Fixe some issue and add more Press enter to continue

--- Installfest_THP.sh
+++ Installfest_THP.sh
@@ -23,34 +23,8 @@ setup_color() {
     fi
 }
 
-ask_install_gem() {
-    set -- $(locale LC_MESSAGES)
-    yesptrn="$1"
-    noptrn="$2"
-    yesword="$3"
-    noword="$4"
-    while true; do
-
-        echo ""
-        read -p "${YELLOW} Install the Gem "$gem" ? [${yesword}/${noword}]? ${RESET}" yn
-        case $yn in
-        ${yesptrn##^})
-            gem install "$gem"
-            break
-            ;;
-        ${yesword##^})
-            gem install "$gem"
-            break
-            ;;
-        ${noptrn##^}) break ;;
-        ${noword##^}) break ;;
-        *) echo "Answer ${yesword} / ${noword}." ;;
-        esac
-    done
-}
-
 press_any_key_to_continue() {
-    read -n 1 -s -r -p "${GREEN} Press any key to continue ${RESET}"
+    read -n 1 -s -r -p "${GREEN} Press any key to continue `echo $'\n '`${RESET}"
 }
 
 ask() {
@@ -68,11 +42,14 @@ ask() {
 
 header() {
     clear
+    echo ""
     echo "${YELLOW}#######################################################${RESET}"
     echo ""
     echo "${GREEN}  $1 ${RESET}"
     echo ""
     echo "${YELLOW}#######################################################${RESET}"
+    echo ""
+    echo ""
 }
 
 install_dependencies() {
@@ -90,7 +67,19 @@ install_RVM() {
 
     press_any_key_to_continue
 
-    sudo apt install gnupg
+    if hash gpg 2>/dev/null; then
+        echo "GPG is detected on your system"
+        echo "I Will install RVM"
+        press_any_key_to_continue
+    else
+        echo "GPG is not detected in your system"
+        echo "I need it for install RVM"
+        echo "I will install it with sudo"
+        press_any_key_to_continue
+        sudo apt install gnupg
+        echo "Now I will install RVM"
+        press_any_key_to_continue
+    fi
     gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
     curl -L get.rvm.io | bash -s stable
 
@@ -124,78 +113,144 @@ install_Heroku() {
     header "Heroku installation"
 
     press_any_key_to_continue
+
+    echo "I start install of HEROKU with CURL"
     curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
+
     press_any_key_to_continue
+}
+
+ask_install_gem() {
+    set -- $(locale LC_MESSAGES)
+    yesptrn="$1"
+    noptrn="$2"
+    yesword="$3"
+    noword="$4"
+    while true; do
+
+        echo ""
+        read -p "${YELLOW} Install the Gem "$gem" ? [${yesword}/${noword}]? ${RESET}" yn
+        case $yn in
+        ${yesptrn##^})
+            gem install "$gem"
+            break
+            ;;
+        ${yesword##^})
+            gem install "$gem"
+            break
+            ;;
+        ${noptrn##^}) break ;;
+        ${noword##^}) break ;;
+        *) echo "Answer ${yesword} / ${noword}." ;;
+        esac
+    done
 }
 
 install_all_gem() {
     header "Gem installation"
 
+    press_any_key_to_continue
+
     gem_array=(rspec rubocop pry dotenv twitter nokogiri launchy watir selenium-webdriver json colorize sinatra shotgun csv rack sqlite3 faker)
+    echo "Here is the list of all gems that The Hacking Project offers"
+    echo ${gem_array[*]}
+    echo "you will be able to choose which ones you want to install"
+    press_any_key_to_continue
 
     for gem in "${gem_array[@]}"; do ask_install_gem; done
+
     press_any_key_to_continue
 }
 
 install_gem_pg() {
     header "PG's gem installation"
 
+    press_any_key_to_continue
+
     if [[ -r /etc/os-release ]]; then
         . /etc/os-release
         if [[ $ID == ubuntu ]]; then
             read _ UBUNTU_VERSION_NAME <<<"$VERSION"
             echo "Running Ubuntu $UBUNTU_VERSION_NAME"
+            echo "I will add 'deb http://apt.postgresql.org/pub/repos/apt/ $UBUNTU_CODENAME-pgdg main' in /etc/apt/sources.list.d/pgdg.list"
+            press_any_key_to_continue
             sudo sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ $UBUNTU_CODENAME-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
-            echo "I add 'deb http://apt.postgresql.org/pub/repos/apt/ $UBUNTU_CODENAME-pgdg main' in /etc/apt/sources.list.d/pgdg.list"
+            press_any_key_to_continue
         else
             echo "Not running an Ubuntu distribution. ID=$ID, VERSION=$VERSION"
-            sudo sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ $UBUNTU_CODENAME-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
+            echo "Your system is based on Ubuntu $UBUNTU_CODENAME"
             echo "I add 'deb http://apt.postgresql.org/pub/repos/apt/ $UBUNTU_CODENAME-pgdg main' in /etc/apt/sources.list.d/pgdg.list"
+            press_any_key_to_continue
+            sudo sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ $UBUNTU_CODENAME-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
+            press_any_key_to_continue
         fi
-    else
-        echo "Not running a distribution with /etc/os-release available"
-    fi
     wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
     sudo apt update
     sudo apt upgrade
     sudo apt install postgresql-common
     sudo apt install postgresql-9.5 libpq-dev
+    else
+        echo "Not running a distribution with /etc/os-release available"
+        echo "I can't install the GEM PG with PostgreSQL"
+    fi
 
     press_any_key_to_continue
 }
 
-check() {
+check_ror_version() {
     ruby_version=$(ruby -v)
     rails_version=$(rails -v)
     echo ${ruby_version}
     if [ $ruby_version = "ruby 2.5.1p57 (2018-03-29 revision 63029) [x86_64-linux]" ]; then
-        echo "${GREEN} you have the right version of Ruby ${RESET}"
+        echo "${GREEN} you have the right version of Ruby for The Hacking Project ${RESET}"
+        press_any_key_to_continue
     else
-        echo "${RED} You have not the right version of Ruby ${RESET}"
+        echo "${RED} You have not the right version of Ruby for The Hacking Project ${RESET}"
+        press_any_key_to_continue
     fi
 
     echo ${rails_version}
     if [ $rails_version = "Rails 5.2.3" ]; then
-        echo "${GREEN} you have the right version of Rails ${RESET}"
+        echo "${GREEN} you have the right version of Rails for The Hacking Project ${RESET}"
+        press_any_key_to_continue
     else
-        echo "${RED} You have not the right version of Rails ${RESET}"
+        echo "${RED} You have not the right version of Rails for The Hacking Project ${RESET}"
+        press_any_key_to_continue
+    fi
+
+    if [ $ruby_version = "ruby 2.5.1p57 (2018-03-29 revision 63029) [x86_64-linux]" ] || [ $rails_version = "Rails 5.2.3" ]; then
+        echo "You have the right versions of Ruby and Rails for The Hacking Project"
+        press_any_key_to_continue
+    else
+        echo "Something is wrong"
+        press_any_key_to_continue
     fi
 }
 
 install_oh_my_zsh() {
     header "OH MY ZSH installation"
 
+    press_any_key_to_continue
+
     if hash zsh 2>/dev/null; then
-        return
+        echo "ZSH has been detected on your system"
+        press_any_key_to_continue
     else
         echo "Zsh has not detected on your system"
         echo "I will install it with sudo"
         press_any_key_to_continue
         sudo apt install zsh
     fi
+
     if hash curl 2>/dev/null; then
+        echo "CURL has been detected on your system"
+        echo "I will install OH MY ZSH with it"
+        press_any_key_to_continue
         sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     elif hash wget 2>/dev/null; then
+        echo "WGET has been detected on your system"
+        echo "I will install OH MY ZSH with it"
+        press_any_key_to_continue
         sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
     else
         echo "CURL and WGET has not detected on your system"
@@ -203,6 +258,8 @@ install_oh_my_zsh() {
         echo "before install Oh-My-Zsh"
         press_any_key_to_continue
         sudo apt install curl
+        echo "Now I will install OH MY ZSH with CURL"
+        press_any_key_to_continue
         sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     fi
     press_any_key_to_continue
@@ -211,22 +268,27 @@ install_oh_my_zsh() {
 install_vim() {
     header "VIM installation"
 
+    press_any_key_to_continue
+
     if hash vim 2>/dev/null; then
-        echo "Vim is already install"
+        echo "Vim is already installed"
         press_any_key_to_continue
     else
         echo "Vim has not detected on your system"
         echo "I will install it with sudo"
         press_any_key_to_continue
         sudo apt install vim
+        press_any_key_to_continue
     fi
 }
 
 Install_vscode() {
     header "Visual Code Installation"
 
+    press_any_key_to_continue
+
     if hash code 2>/dev/null; then
-        echo "Visual Code is already install"
+        echo "Visual Code is already installed"
         press_any_key_to_continue
     else
         echo "Visual code has not detected on your system"
@@ -240,62 +302,14 @@ Install_vscode() {
             sudo apt update
             sudo apt install snapd
         fi
+        echo "I will install Visual Code with SNAP"
+        press_any_key_to_continue
+
         snap install code --classic
+        
+        press_any_key_to_continue
     fi
 }
-
-# menu_old() {
-#     clear
-#     echo "#######################################"
-#     echo "################# menu ################"
-#     echo "#######################################"
-#     echo "     1: Dependencies installation"
-#     echo "     2: RVM installation"
-#     echo "     3: Ruby version 2.5.1 installation"
-#     echo "     4: Rails version 2.5.3 installation"
-#     echo "     5: Heroku Installation"
-#     echo "     6: Gem Installation"
-#     echo "     7: PG's gem installation"
-#     echo "     q: quit"
-#     echo "#######################################"
-#     echo "                             "
-#     read -p "Enter your choice: " choice
-#     case "$choice" in
-#     1)
-#         install_dependencies
-#         menu
-#         ;;
-#     2)
-#         install_RVM
-#         menu
-#         ;;
-#     3)
-#         install_Ruby
-#         menu
-#         ;;
-#     4)
-#         install_Rails
-#         menu
-#         ;;
-#     5)
-#         install_Heroku
-#         menu
-#         ;;
-#     6)
-#         install_all_gem
-#         menu
-#         ;;
-#     7)
-#         install_gem_pg
-#         check
-#         menu
-#         ;;
-#     "q" | "q")
-#         echo "See you next time ..."
-#         ;;
-#     *) echo "Bad choice" ;;
-#     esac
-# }
 
 menu_whiptail() {
     while [ 1 ]; do
@@ -335,7 +349,7 @@ menu_whiptail() {
             ;;
         "7)")
             install_gem_pg
-            check
+            check_ror_version
             ;;
         "8)")
             install_oh_my_zsh
